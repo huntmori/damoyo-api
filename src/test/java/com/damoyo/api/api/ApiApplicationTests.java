24405ebecfb2cb13fe3e044a3edd7bb88e8f4d41
huntmori <huntmori04x22@gmail.com>
2021-04-13 23:48:04 +0900

    init and querydsl gradle setting

---
+++ src/test/java/com/damoyo/api/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damoyo.api.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
