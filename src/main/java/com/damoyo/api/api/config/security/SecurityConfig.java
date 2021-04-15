2f8be3396f106050bfb3750c61c8b8fe8d7757a7
huntmori <huntmori04x22@gmail.com>
2021-04-15 23:51:43 +0900

    start security

--- src/main/java/com/damoyo/api/api/config/security/SecurityConfig.java
+++ src/main/java/com/damoyo/api/api/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.damoyo.api.api.config;
+package com.damoyo.api.api.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
